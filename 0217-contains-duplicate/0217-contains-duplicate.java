a83132e4f1deb14ee275331a04bfbf374c3b696d
Kaliyugalaxmi <konarlaxmi04@gmail.com>
2026-08-02 12:25:35 +0530

    Time: 24 ms (27.42%), Space: 81.6 MB (79.78%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        int count=0;
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i++){
+            
+                if(nums[i]==nums[i+1]){
+                   return true;
+                    
+                } 
+                
+                
+            
+        }
+       return false;
+    }
+}
